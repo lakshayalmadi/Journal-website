36fe24d7423e849113300b6c002756e64ac2754c
lakshayalmadi <lashay.almadi.la@gmail.com>
2021-05-26 03:10:16 +0530

    full

--- app.js
+++ app.js
@@ -10,53 +10,59 @@ const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pelle
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
-let posts = [];
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-app.get("/", function(req,res){
+let posts = [];
+
+app.get("/", function(req, res){
   res.render("home", {
     startingContent: homeStartingContent,
     posts: posts
-  });
+    });
 });
 
-app.get("/about", function(req,res){
-  res.render("about", {mainContent: aboutContent});
+app.get("/about", function(req, res){
+  res.render("about", {aboutContent: aboutContent});
 });
 
-app.get("/contact", function(req,res){
+app.get("/contact", function(req, res){
   res.render("contact", {contactContent: contactContent});
 });
+
 app.get("/compose", function(req, res){
   res.render("compose");
 });
 
 app.post("/compose", function(req, res){
-  const post={
-    title : req.body.postTitle,
-    content : req.body.postBody
+  const post = {
+    title: req.body.postTitle,
+    content: req.body.postBody
   };
+
   posts.push(post);
+
   res.redirect("/");
+
 });
 
 app.get("/posts/:postName", function(req, res){
-  const requestedTitle= _.lowerCase(req.params.postName);
-  posts.forEach(function(post){
+  const requestedTitle = _.lowerCase(req.params.postName);
 
+  posts.forEach(function(post){
     const storedTitle = _.lowerCase(post.title);
-    
-    if(storedTitle === requestedTitle){
-      console.log("Match Found");
-    }
-    else{
-      console.log("Not a match");
+
+    if (storedTitle === requestedTitle) {
+      res.render("post", {
+        title: post.title,
+        content: post.content
+      });
     }
   });
+
 });
 
 app.listen(3000, function() {
