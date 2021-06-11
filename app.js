8a663a5dbc7333af90f2b6a13e60537aaa7fc573
lakshayalmadi <lashay.almadi.la@gmail.com>
2021-06-11 20:21:57 +0530

    updated-content

--- app.js
+++ app.js
@@ -5,9 +5,8 @@ const _ = require("lodash");
 const mongoose = require('mongoose');
 
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+const homeStartingContent = "This website is to post about your ideas or how your day went. You can add your name to the post or even leave it for it to be anonymous. Click on compose to add your post and to know more about me check out the about section.";
+const aboutContent = "Hello, I am Lakshay Almadi. A pre-final year student at Vellore Institute of Technology. I am a machine learning enthusiast and a web developer currently open to work. You can check out my github in the bottom link or reach out to me through mail.";
 
 const app = express();
 
@@ -20,6 +19,7 @@ mongoose.connect('mongodb://localhost:27017/blogDB', {useNewUrlParser: true, use
 
 const postSchema = {
   title: String,
+  author: String,
   content: String
 };
 
@@ -39,11 +39,10 @@ app.get("/", function(req, res){
   
 });
 
-
-
 app.post("/compose", function(req, res){
   const post = new Post({
     title: req.body.postTitle,
+    author: req.body.postAuthor,
     content: req.body.postBody
   });
 
@@ -56,12 +55,12 @@ app.post("/compose", function(req, res){
 });
 
 app.get("/posts/:postID", function(req, res){
-  const requestedTitle = _.lowerCase(req.params.postName);
   const requestedPostId = req.params.postID;
 
   Post.findOne({_id: requestedPostId}, function(err, post){
     res.render("post", {
       title: post.title,
+      author: post.author,
       content: post.content
     });
   })
@@ -71,9 +70,6 @@ app.get("/about", function(req, res){
   res.render("about", {aboutContent: aboutContent});
 });
 
-app.get("/contact", function(req, res){
-  res.render("contact", {contactContent: contactContent});
-});
 
 app.get("/compose", function(req, res){
   res.render("compose");
